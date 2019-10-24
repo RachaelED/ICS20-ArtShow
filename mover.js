5b4e862d4b547a37bb919ac59714db214e414636
RachaelED <55112454+RachaelED@users.noreply.github.com>
2019-10-24 17:30:11 -0400

    Add comments to code

--- mover.js
+++ mover.js
@@ -13,7 +13,9 @@ Mover.prototype.show = function(){
    
     fill(random(40)%100,random(255)%10,random(255)%100);
     noStroke();
+  // change x and y coordinates, add random for width and height
     ellipse(this.x - this.x/3, this.y - this.y/7,random(20, 30),random(20, 30));
+  // create a group of squares, as well as circles
   fill(random(255)%100,random(255)%10,random(40)%100);
   rect(this.x, this.y, random(30, 50), random(30, 50));
 };
