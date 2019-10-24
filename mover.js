97e07e7d46b2c886e1251f268e8e3d7955e99b53
RachaelED <55112454+RachaelED@users.noreply.github.com>
2019-10-24 17:20:33 -0400

    Create mover.js file

---
+++ mover.js
@@ -0,0 +1,19 @@
+var Mover = function(){
+  this.x= 400/4;
+  this.y = 400/4;
+  
+};
+
+Mover.prototype.update = function(){
+    this.x+=random(-3,5);
+    this.y +=random(-3,4);
+};
+
+Mover.prototype.show = function(){
+   
+    fill(random(40)%100,random(255)%10,random(255)%100);
+    noStroke();
+    ellipse(this.x - this.x/3, this.y - this.y/7,random(20, 30),random(20, 30));
+  fill(random(255)%100,random(255)%10,random(40)%100);
+  rect(this.x, this.y, random(30, 50), random(30, 50));
+};
